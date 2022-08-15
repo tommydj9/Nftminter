90c6973ced16dfd1ff17e6ad72224aa5152bed3f
tommydj9 <tommasozucca6@gmail.com>
2022-08-15 08:37:45 +0200

    footer

--- minter/pages/index.js
+++ minter/pages/index.js
@@ -22,7 +22,7 @@ export default function Home() {
     <div className="flex w-screen h-screen items-center justify-center">
       <Head>
         <title>NFT Minter</title>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/images/logo.png" />
 
       </Head>
 
@@ -120,7 +120,9 @@ export default function Home() {
           <div class="mb-6 md:mb-0">
             <a href="https://flowbite.com/" class="flex items-center">
               <img src="/images/logo.png" class="mr-3 h-8" alt="FlowBite Logo" img />
+
               <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">NftMinter</span>
+              <h1 className="testo">hd</h1>
             </a>
           </div>
           <div class="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3">
@@ -163,8 +165,7 @@ export default function Home() {
         <div class="sm:flex sm:items-center sm:justify-between">
           <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2022 <a href="https://flowbite.com/" class="hover:underline">NftMinter™</a>. All Rights Reserved.
           </span>
-          <br></br>
-          <br></br>
+          <h1 className="testo">sh</h1>
           <div class="flex mt-4 space-x-6 sm:justify-center sm:mt-0">
             <a href="#" class="text-gray-500 hover:text-gray-900 dark:hover:text-white">
               <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true"><path fill-rule="evenodd" d="M22 12c0-5.523-4.477-10-10-10S2 6.477 2 12c0 4.991 3.657 9.128 8.438 9.878v-6.987h-2.54V12h2.54V9.797c0-2.506 1.492-3.89 3.777-3.89 1.094 0 2.238.195 2.238.195v2.46h-1.26c-1.243 0-1.63.771-1.63 1.562V12h2.773l-.443 2.89h-2.33v6.988C18.343 21.128 22 16.991 22 12z" clip-rule="evenodd"></path></svg>
